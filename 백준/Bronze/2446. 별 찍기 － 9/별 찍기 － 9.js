ab4f9e115663b9934820d46ba3551125aaed2de2
ZEONe <103362856+zeonzeon-e@users.noreply.github.com>
2024-01-23 16:42:14 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 212 ms, Memory: 11324 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.js
@@ -0,0 +1,48 @@
+//https://www.acmicpc.net/problem/2444
+//별 찍기 - 7
+
+const readline= require("readline");
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+var input = new Array();
+rl.on("line", (line) => {
+    input = line.split(' ');
+    solution(input)
+    rl.close();
+})
+
+rl.on('close', ()=> {
+    process.exit();
+})
+
+function solution(n)
+{
+    let answer = "";
+    let blank = "";
+    for(let i = n; i >= 1; i--){
+        for(let j=1; j <= n-i; j++){
+            blank += " ";
+        }
+        for(let j = 1; j <= 2*i-1; j++){
+            answer += "*";
+        }
+        console.log(blank + answer)
+        answer = ""
+        blank = ""
+    }
+    for(let i = 2; i <= n; i++){
+        for(let j=1; j <= n-i; j++){
+            blank += " ";
+        }
+        for(let j = 1; j <= 2*i-1; j++){
+            answer += "*";
+        }
+        console.log(blank + answer)
+        answer = ""
+        blank = ""
+    }
+}
